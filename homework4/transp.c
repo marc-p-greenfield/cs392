b6b7dbcf5a1360be94faad9883ec8ff9554265cc
Marc Greenfield <mgreenf1@stevens.edu>
2018-11-16 22:53:33 -0500

    added comments

--- homework4/transp.c
+++ homework4/transp.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <sys/time.h>
 
-
+//Printing utility function for debugging matrix transposition
 void matrixPrint(int input_width, float* matrix)
 {
    printf("Matrix:\n");
@@ -13,6 +13,7 @@ void matrixPrint(int input_width, float* matrix)
         printf("%f ",matrix[count]);
     }
 }
+
 int main (int argc, char* argv[])
 {
     int input_width = atoi(argv[1]);
@@ -22,12 +23,14 @@ int main (int argc, char* argv[])
     float* input_matrix = (float*)malloc(input_width*input_width * sizeof(float));
     float* output_matrix = (float*)malloc(input_width*input_width * sizeof(float));
 
+    //Populate the input matrix with random float values
     srand(time(NULL));
     for (int count = 0; count < input_width*input_width; count++)
     {
         input_matrix[count] = rand();
     }
 
+    //Begin timing of transposition
     gettimeofday(&start, NULL);
     for (int i = 0; i < input_width; i+=block_width){
         for (int j = 0; j < input_width; j+=block_width){
@@ -39,9 +42,11 @@ int main (int argc, char* argv[])
         }
     }
     gettimeofday(&end, NULL);
+    //End timing of transposition
 
     printf("Transposition time: %ld\n",((end.tv_sec * 1000000 + end.tv_usec)-(start.tv_sec * 1000000 + start.tv_usec)));
 
+    //Return arrays to the heap
     free(input_matrix);
     free(output_matrix);
 
